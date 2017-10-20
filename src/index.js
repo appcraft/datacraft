8c12f1cd153cd71f455bc6310783db8b52f1b4e6
Gregory Potdevin <gregory.potdevin@gmail.com>
2017-10-20 08:55:21 +0200

    fix scrollbar visibility

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React, {Component} from 'react'
+import DataTable from './DataTable'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+export {
+  DataTable
 }
